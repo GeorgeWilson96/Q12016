f65edadbfb95fb109eece9d5551de834f04ac5cd
George Wilson <george.wilson.2905@gmail.com>
2017-05-16 18:11:10 +0100

    changed to Q12015

--- src/Q12016/Main.java
+++ src/Q12016/Main.java
@@ -3,11 +3,7 @@ package Q12016;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        CarsCard carsCard = new CarsCard("Aston Martin");
-        BikesCard bikesCard = new BikesCard("Generic Bike");
-        carsCard.writeCard();
-        bikesCard.writeCard();
-        carsCard.compare(bikesCard,0);
+	    // write your code here
+        
     }
 }
